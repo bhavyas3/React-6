900655df9ae188cd94be0df339ad6ef9b946fce2
bhavyas3 <150443103+bhavyas3@users.noreply.github.com>
2023-11-10 18:51:13 +0530

    Add files via upload

---
+++ src/Components/Education.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section>
+                <h2>Education</h2>
+
+                <h3>Vellore Institute of Technology, Vellore</h3>
+                <p>B.tech (Computer Science Engineering spl in Internet of Things)</p>
+                <p>Graduation Year: 2025</p>
+
+                <h3>DPS RK Puram</h3>
+                <p>High School</p>
+                <p>Graduation Year: 2020</p>
+
+                <h3>DPS RK Puram</h3>
+                <p>Elementary School</p>
+                <p>Graduation Year: 2018</p>
+            
+            
+    </section>
+  );
+}
+
+export default Education;
+
